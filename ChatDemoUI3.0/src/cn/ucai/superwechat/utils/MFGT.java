caa13c72382b7dc245dfcde8f08f4dc429869ecc
xiaomiao <13227036990@163.com>
2016-11-08 14:17:51 +0800

    自定义PopUpWindow,并添加自定义控件

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.AddContactActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
 import cn.ucai.superwechat.ui.SettingsActivity;
@@ -49,5 +50,9 @@ public class MFGT {
 
     public static void gotoUserProfile(Activity context) {
         startActivity(context, UserProfileActivity.class);
+    }
+
+    public static void gotoAddFirent(Activity context){
+        startActivity(context, AddContactActivity.class);
     }
 }
